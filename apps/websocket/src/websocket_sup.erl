c53e663e9065addb584966e32fa66e58240295d8
Koen <koenkesseler@bluenotion.nl>
2016-06-03 16:05:37 +0200

    Removed unnessesary comments and edoc

--- apps/websocket/src/websocket_sup.erl
+++ apps/websocket/src/websocket_sup.erl
@@ -1,6 +1,3 @@
-%% Feel free to use, reuse and abuse the code in this file.
-
-%% @private
 -module(websocket_sup).
 -behaviour(supervisor).
 
@@ -13,8 +10,6 @@
 -spec start_link() -> {ok, pid()}.
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
-
-%% supervisor.
 
 init([]) ->
     Procs = [],
